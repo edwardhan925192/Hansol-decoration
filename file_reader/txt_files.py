8771c783550dd4cc87ad82b70c925e26a54ac313
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2024-02-12 20:38:32 +0900

    Update txt_files.py

--- file_reader/txt_files.py
+++ file_reader/txt_files.py
@@ -1,5 +1,6 @@
 import os
 from langchain_community.document_loaders.text import TextLoader
+from tqdm import tqdm
 
 def clean_text_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -32,22 +33,23 @@ def txts_to_docs(folder_path):
     # Initialize an empty list to hold the cleaned documents
     cleaned_docs = []
 
-    # List all files in the given folder
-    for filename in os.listdir(folder_path):
-        # Check if the file is a .txt file
-        if filename.endswith('.txt'):
-            # Construct the full file path
-            file_path = os.path.join(folder_path, filename)
-            
-            # Read and clean the file content
-            with open(file_path, 'r', encoding='utf-8') as file:
-                content = file.read()
-                # Replace multiple newlines with a single space
-                cleaned_content = ' '.join(content.splitlines())
-                # Replace multiple spaces with a single space
-                cleaned_content = ' '.join(cleaned_content.split())
-            
-            # Add the cleaned content to the list
-            cleaned_docs.append(cleaned_content)
+    # Retrieve a list of all .txt files in the folder
+    txt_files = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+    
+    # List all .txt files in the given folder and wrap it with tqdm for a progress bar
+    for filename in tqdm(txt_files, desc='Processing TXTs'):
+        # Construct the full file path
+        file_path = os.path.join(folder_path, filename)
+        
+        # Read and clean the file content
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            # Replace multiple newlines with a single space
+            cleaned_content = ' '.join(content.splitlines())
+            # Replace multiple spaces with a single space
+            cleaned_content = ' '.join(cleaned_content.split())
+        
+        # Add the cleaned content to the list
+        cleaned_docs.append(cleaned_content)
 
     return cleaned_docs
